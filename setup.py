acaeb9da883809fade8dc91b2cb3ce2dbe3f3d64
Zouheir Nakouzi <zouheir2002@gmail.com>
2023-03-23 17:07:54 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name='popupsort',
-    version='1.1',
+    version='1.1.1',
     description='A Python package for visualizing sorting algorithms',
     long_description = README,
     long_description_content_type="text/markdown",
