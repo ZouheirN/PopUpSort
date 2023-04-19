9bdac8047610c9abe6a4688ece0c02c5f60434e7
Zouheir Nakouzi <zouheir2002@gmail.com>
2023-04-19 18:03:04 +0300

    the window now closes automatically for when comparing algorithms

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name='popupsort',
-    version='1.5.4',
+    version='1.6',
     description='A Python package for visualizing sorting algorithms',
     long_description=README,
     long_description_content_type="text/markdown",
